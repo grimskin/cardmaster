fe2e3c72e9b70f36bb84f1b3395a30b59f49b9bd
grimskin <grimskin@gmail.com>
2019-12-23 09:14:09 +0100

    default controller now returns html for react app

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,11 +5,26 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends AbstractController
 {
     public function index()
     {
-        die('lel');
+        return new Response(
+            <<<HTML
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <link rel="stylesheet" href="build/app.css">
+    <title>--</title>
+</head>
+<body>
+    <script src="build/runtime.js"></script>
+    <script src="build/app.js"></script>
+</body>
+</html>
+HTML
+        );
     }
 }
