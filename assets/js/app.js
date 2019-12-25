52b9825f2c0699cc5bec605daf5a1e69626ad9c5
grimskin <grimskin@gmail.com>
2019-12-25 21:06:36 +0100

    testing react integration

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,12 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
 // any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../css/app.css';
+
+console.log('Hello?');
+
+const domContainer = document.querySelector('#root');
+ReactDOM.render(<h1>Ehlo</h1>, domContainer);
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -16,11 +16,13 @@ class DefaultController extends AbstractController
 <!DOCTYPE html>
 <html lang="en">
 <head>
-     <link rel="stylesheet" href="build/app.css">
+    <link rel="stylesheet" href="build/app.css">
     <title>--</title>
 </head>
 <body>
+    <div id="root"></div>
     <script src="build/runtime.js"></script>
+    <script src="build/vendors~app.js"></script>
     <script src="build/app.js"></script>
 </body>
 </html>
