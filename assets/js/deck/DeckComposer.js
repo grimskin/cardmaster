460715bbb7785d8ad2b8158d879acd8254295aed
grimskin <grimskin@gmail.com>
2019-12-30 20:53:59 +0100

    basic styles for autocomplete

--- assets/js/deck/DeckComposer.js
+++ assets/js/deck/DeckComposer.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
 
 class DeckComposer extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
     render() {
-        return (<div>Deck Composer</div>);
+        return (
+            <div>
+                Deck Composer
+                <div>
+                    <div className={"autocomplete"}>
+                        <input type="text" id="card_name_input"/>
+                        <button>add</button>
+                        <div className={"ac_items"}>
+                            <div className={"ac_option"}>Angel of Grace</div>
+                            <div className={"ac_option"}>Angelic Exaltation</div>
+                            <div className={"ac_option"}>Archway Angel</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 
