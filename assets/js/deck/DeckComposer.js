32a095a501f5fdefdb4b871cdda47f26e67753ed
grimskin <grimskin@gmail.com>
2019-12-31 12:20:36 +0100

    fixed classname references

--- assets/js/deck/DeckComposer.js
+++ assets/js/deck/DeckComposer.js
@@ -12,13 +12,13 @@ class DeckComposer extends Component {
             <div>
                 Deck Composer
                 <div>
-                    <div className={"autocomplete"}>
+                    <div className="autocomplete">
                         <input type="text" id="card_name_input"/>
                         <button>add</button>
-                        <div className={"ac_items"}>
-                            <div className={"ac_option"}>Angel of Grace</div>
-                            <div className={"ac_option"}>Angelic Exaltation</div>
-                            <div className={"ac_option"}>Archway Angel</div>
+                        <div className="ac_items">
+                            <div className="ac_option">Angel of Grace</div>
+                            <div className="ac_option">Angelic Exaltation</div>
+                            <div className="ac_option">Archway Angel</div>
                         </div>
                     </div>
                 </div>
