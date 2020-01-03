b721a50a120fcc1e85eda00c66dacc0ea7d54f9d
grimskin <grimskin@gmail.com>
2020-01-03 22:57:32 +0100

    controls for condition picker

--- assets/js/condition/ConditionPicker.js
+++ assets/js/condition/ConditionPicker.js
@@ -1,8 +1,48 @@
 import React, { Component } from 'react';
+import AcInput from "../common/AcInput";
 
 class ConditionPicker extends Component {
+    constructor(props) {
+        super(props);
+
+        this.acInput = React.createRef();
+        this.updateConditionParam = this.updateConditionParam.bind(this);
+        this.confirmParam = this.confirmParam.bind(this);
+        this.addCondition = this.addCondition.bind(this);
+    }
+
+    updateConditionParam(value) {
+
+    }
+
+    confirmParam() {
+
+    }
+
+    addCondition() {
+
+    }
+
     render() {
-        return (<div>Condition Picker</div>);
+        return (
+            <div id="condition-picker">
+                Condition Picker
+                <br />
+                <select>
+                    <option>--</option>
+                    {this.props.conditions.map((item, i) => {
+                        return <option value={item.name} key={i}>{item.title}</option>
+                    })}
+                </select>
+                <AcInput id="condition-picker"
+                         ref={this.acInput}
+                         cards={this.props.cards}
+                         updateCardCallback={this.updateConditionParam}
+                         addCardCallback={this.confirmParam}
+                />
+                <button onClick={this.addCondition}>add</button>
+            </div>
+        );
     }
 }
 
