5c93ed73d604e83892bb3c7dcd15ded9881e10e7
grimskin <grimskin@gmail.com>
2021-07-07 20:02:06 +0200

    added twig

---
+++ src/Controller/SetStatsController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class SetStatsController extends AbstractController
+{
+    public function setStats(string $set): Response
+    {
+        return $this->render('set_stats.html.twig', [
+            'set' => $set,
+        ]);
+    }
+}
