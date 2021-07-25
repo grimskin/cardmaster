a45e5f16096d5a1b036ec8717ef5a3c52e61454f
grimskin <grimskin@gmail.com>
2021-07-25 20:33:21 +0200

    routing update

--- src/Controller/SetStatsController.php
+++ src/Controller/SetStatsController.php
@@ -7,6 +7,7 @@ namespace App\Controller;
 use App\Service\DataLoader;
 use App\Service\Set\StatsCollector;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class SetStatsController extends AbstractController
@@ -18,6 +19,13 @@ class SetStatsController extends AbstractController
     {
         $this->collector = $collector;
         $this->dataLoader = $dataLoader;
+    }
+
+    public function index(): Response
+    {
+        return new RedirectResponse(
+            $this->generateUrl('set_view', ['set' => 'AFR'])
+        );
     }
 
     public function setStats(string $set): Response
