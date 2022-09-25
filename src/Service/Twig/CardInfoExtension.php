dd68a7ae6cb7f8426a896ba1ee101cf0c5d0e996
grimskin <grimskin@gmail.com>
2022-09-25 20:15:04 +0200

    added card_info twig extension for articles

---
+++ src/Service/Twig/CardInfoExtension.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace App\Service\Twig;
+
+
+use Twig\Extension\AbstractExtension;
+use Twig\Markup;
+use Twig\TwigFunction;
+
+class CardInfoExtension extends AbstractExtension
+{
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('card_info', [$this, 'cardInfo']),
+        ];
+    }
+
+    public function cardInfo(string $cardName): string
+    {
+        return $this->canonizeCardName($cardName);
+    }
+
+    private function canonizeCardName(string $cardName): string
+    {
+        $canonizedName = strtolower($cardName);
+        $canonizedName = str_replace(
+            [',', ' ', 'ü'],
+            ['', '-', 'u'],
+            $canonizedName
+        );
+
+        return new Markup('<a href="/images/cards/'.$canonizedName.'.png" target="_blank">'.$cardName.'</a>', 'UTF-8');
+//        return '<a href="/images/cards/'.$canonizedName.'.png" target="_blank">'.$cardName.'</a>';
+    }
+}
